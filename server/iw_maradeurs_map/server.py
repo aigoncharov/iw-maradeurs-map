d2f0f24da9b3eabd021fe19c3bae22831b4b9e5b
aigoncharov <andrey@goncharov.page>
2024-09-16 11:36:12 +0300

    Add GET /users

--- server/iw_maradeurs_map/server.py
+++ server/iw_maradeurs_map/server.py
@@ -5,16 +5,11 @@ logging.basicConfig(level="DEBUG")
 
 app = Quart(__name__)
 
+users = {"42": {"id": "42", "position": None}}
 
-@app.route("/")
-async def root():
-    logging.debug("GET /root %s", bool(client.qr_code_url))
 
-    if client.qr_code_url is not None:
-        qr_code_image = get_qr_code_image(client.qr_code_url)
-        return await render_template("qr_code.html", qr_code=qr_code_image)
-
-    feeds = await Feed.all()
-    logging.debug("GET /root -> feeds %s", len(feeds))
-
-    return await render_template("feeds.html", user=client.user, feeds=feeds)
+@app.route("/users")
+async def users_get():
+    logging.debug("GET /users %s")
+    users_list = [user for user in users]
+    return {"users": users_list}
