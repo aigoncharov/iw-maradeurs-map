20feec3514fd27e4fd3e9c219705ddc3577942d8
aigoncharov <andrey@goncharov.page>
2024-09-16 11:39:44 +0300

    Add GET /map

--- server/iw_maradeurs_map/server.py
+++ server/iw_maradeurs_map/server.py
@@ -6,10 +6,17 @@ logging.basicConfig(level="DEBUG")
 app = Quart(__name__)
 
 users = {"42": {"id": "42", "position": None}}
+sensors = [{"id": "tbd1"}, {"id": "tbd2"}, {"id": "tbd3"}]
 
 
 @app.route("/users")
 async def users_get():
-    logging.debug("GET /users %s")
+    logging.debug("GET /users")
     users_list = [user for user in users]
     return {"users": users_list}
+
+
+@app.route("/map")
+async def map_get():
+    logging.debug("GET /map")
+    return {"sensors": sensors}
