0c7d890b46f9ac37f1fd8cce0cee2ae25096e327
aigoncharov <andrey@goncharov.page>
2024-09-16 11:49:06 +0300

    Add POST /location

--- server/iw_maradeurs_map/server.py
+++ server/iw_maradeurs_map/server.py
@@ -1,4 +1,4 @@
-from quart import Quart
+from quart import Quart, request
 import logging
 
 logging.basicConfig(level="DEBUG")
@@ -6,7 +6,11 @@ logging.basicConfig(level="DEBUG")
 app = Quart(__name__)
 
 users = {"42": {"id": "42", "position": None}}
-sensors = [{"id": "tbd1"}, {"id": "tbd2"}, {"id": "tbd3"}]
+sensors = [
+    {"id": "tbd1", "position": {"x": 0, "y": 0}},
+    {"id": "tbd2", "position": {"x": 0, "y": 0}},
+    {"id": "tbd3", "position": {"x": 0, "y": 0}},
+]
 
 
 @app.route("/users")
@@ -20,3 +24,16 @@ async def users_get():
 async def map_get():
     logging.debug("GET /map")
     return {"sensors": sensors}
+
+
+@app.route("/location", methods=["POST"])
+async def location_post():
+    logging.debug("POST /location")
+    data = await request.get_json()
+    sensors_signal = data["sensors"]
+    triangulate(sensors_signal)
+    return None, 204
+
+
+def triangulate(sensors_signal):
+    pass
