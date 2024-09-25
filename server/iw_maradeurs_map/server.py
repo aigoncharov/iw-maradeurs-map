7736b8412107880823a40181d8017f3055b4da3a
Alexander Gazaryan <minority169@yandex.ru>
2024-09-25 13:02:02 +0300

    triangulation added

--- server/iw_maradeurs_map/server.py
+++ server/iw_maradeurs_map/server.py
@@ -11,20 +11,35 @@ app = cors(app, allow_origin="*")
 
 users = {"42": {"id": "42", "position": {"x": 37.35960, "y": 55.69850}}}
 
-sensors = [
-    {
-        "id": "MARADEUR1",
-        "position": {"x": 37.35950, "y": 55.69854},
+sensors = {
+    "MARADEUR1": {
+        "x": 37.35950,
+        "y": 55.69854,
     },
-    {
-        "id": "MARADEUR2",
-        "position": {"x": 37.35962, "y": 55.69854},
+    "MARADEUR2": {
+        "x": 37.35962,
+        "y": 55.69854,
     },
-    {
-        "id": "MARADEUR3",
-        "position": {"x": 37.35958, "y": 55.69844},
-    },
-]
+    "MARADEUR3": {
+        "x": 37.35958,
+        "y": 55.69844,
+    }
+}
+
+# sensors = [
+#     {
+#         "id": "MARADEUR1",
+#         "position": {"x": 37.35950, "y": 55.69854},
+#     },
+#     {
+#         "id": "MARADEUR2",
+#         "position": {"x": 37.35962, "y": 55.69854},
+#     },
+#     {
+#         "id": "MARADEUR3",
+#         "position": {"x": 37.35958, "y": 55.69844},
+#     },
+# ]
 
 running_task = None
 cnt = 0
@@ -80,10 +95,60 @@ async def location_post():
     logging.debug("POST /location")
     data = await request.get_json()
     logging.debug(f"POST /location -> data {json.dumps(data)}")
-    sensors_signal = data["sensors"]
-    triangulate(sensors_signal)
+    sensors_signal = adjust_data(data["sensors"])
+    location_x, location_y = triangulate(sensors_signal)
+    logging.debug(f"FOUND NEW POSITION {location_x}, {location_y}")
     return "", 204
 
+def adjust_data(signals):
+    for i in range(len(signals)):
+        idx = signals[i]['id']
+        signals[i]['x'] = sensors[idx]['x']
+        signals[i]['y'] = sensors[idx]['y']
+    return signals
 
-def triangulate(sensors_signal):
-    pass
+def get_distance(signal):
+    return signal + 127 + 100
+
+def triangulate(signals):
+    assert len(signals) >= 2, "Should be at least 2 signals"
+
+    signals.sort(key = lambda x : -x['signal'])
+    
+    x1 = signals[0]['x']
+    y1 = signals[0]['y']
+    dist1 = get_distance(signals[0]['signal'])
+
+    x2 = signals[1]['x']
+    y2 = signals[1]['y']
+    dist2 = get_distance(signals[1]['signal'])
+
+    if len(signals) == 2:
+        coef1 = dist2 / (dist1 + dist2) / (((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) ** 0.5)
+        coef2 = dist1 / (dist1 + dist2) / (((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) ** 0.5)
+        res_x1 = x1 * coef1 + x2 * coef2
+        res_y1 = y1 * coef1 + y2 * coef2
+        return res_x1, res_y1
+
+    x3 = signals[2]['x']
+    y3 = signals[2]['y']
+    dist3 = get_distance(signals[2]['signal'])
+
+    coef1 = dist2 / (dist1 + dist2) / (((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) ** 0.5)
+    coef2 = dist1 / (dist1 + dist2) / (((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) ** 0.5)
+    res_x1 = x1 * coef1 + x2 * coef2
+    res_y1 = y1 * coef1 + y2 * coef2
+
+    coef1 = dist3 / (dist2 + dist3) / (((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3)) ** 0.5)
+    coef2 = dist2 / (dist2 + dist3) / (((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3)) ** 0.5)
+    res_x2 = x2 * coef1 + x3 * coef2
+    res_y2 = y2 * coef1 + y3 * coef2
+
+    coef1 = dist3 / (dist1 + dist3) / (((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3)) ** 0.5)
+    coef2 = dist1 / (dist1 + dist3) / (((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3)) ** 0.5)
+    res_x3 = x1 * coef1 + x3 * coef2
+    res_y3 = y1 * coef1 + y3 * coef2
+
+    res_x = (res_x1 + res_x2 + res_x3) / 3
+    res_y = (res_y1 + res_y2 + res_y3) / 3
+    return res_x, res_y
